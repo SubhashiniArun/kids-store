c983dcbae7a2ab9cbdb59e4150113a258b607238
SubhashiniArun <subhashini258@gmail.com>
2023-01-06 19:35:43 -0500

    adding spring cloud server config

---
+++ src/test/java/com/subhacodes/kidsstore/KidsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhacodes.kidsstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
